00af3cfb5d1d49809c603951e6dab5240513a09c
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-10-23 19:41:49 -0500

    created the find my artist function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,17 +13,28 @@ function addToCollection(title, artist, yearPublished, track){
     return album;
 }
 
-console.log(addToCollection('Meet the Woo 2', 'Pop Smoke', '2020', 'Dior'));
-console.log(addToCollection('My Beautiful Dark Twisted Fantasy', 'Kanye', '2010', 'Devil in a new dress'));
-console.log(addToCollection('Astroworld', 'Travis Scott', '2018', 'Sicko mode'));
-console.log(addToCollection('Views', 'Drake', '2016', 'One Dance'));
-console.log(addToCollection('Damn', 'Kendrick Lamar', '2017', 'DNA'));
-console.log(addToCollection('7220', 'Lil Durk', '2022', 'Blocklist'));
-console.log(collection)
+console.log(addToCollection('Meet the Woo 2', 'Pop Smoke', '2020', 'Dior: 2:53'));
+console.log(addToCollection('My Beautiful Dark Twisted Fantasy', 'Kanye', '2010', 'Devil in a new dress: 5:53'));
+console.log(addToCollection('Astroworld', 'Travis Scott', '2018', 'Sicko mode: 5:23'));
+console.log(addToCollection('Views', 'Drake', '2016', 'One Dance: 2:55'));
+console.log(addToCollection('Damn', 'Kendrick Lamar', '2017', 'DNA: 4:46'));
+console.log(addToCollection('7220', 'Lil Durk', '2022', 'Blocklist: 2:20'));
+console.log(collection);
 
 function showCollection(array) {
     console.log(array.length);
     for(let i=0;i<array.length; i++){
             console.log('${array[i].title} by ${array[i].artist} published in ${array[i].yearPublished}${array[i].tracks}')
     }
+}
+showCollection (collection)
+
+function findByArtist(artist) {
+    let results = [];
+    for (title of collection) {
+        if(title.artist === artist) {
+            results.push(title)
+        }
+    }
+    return results;
 }
